5321e8b2ce5c56b870225eaefcc816e593422fbd
Chimi Lhamu Tamang <66162446+chimitamang@users.noreply.github.com>
2020-06-01 19:40:53 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hellojava{
 public static void main(String[] arg){
-sysytem.out.println("Hello World, Chimi form GIT repositery ");
+system.out.println("Hello World, Chimi form GIT repositery ");
 }
 }
